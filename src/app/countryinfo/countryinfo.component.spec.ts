3937a835179bc8b9ad1c2c9d2634adeac97d5a35
md bused <lanx7hacks@gmail.com>
2025-01-22 15:33:23 -0500

    Part C worldmap

---
+++ src/app/countryinfo/countryinfo.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CountryinfoComponent } from './countryinfo.component';
+
+describe('CountryinfoComponent', () => {
+  let component: CountryinfoComponent;
+  let fixture: ComponentFixture<CountryinfoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CountryinfoComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CountryinfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
