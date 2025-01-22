3d48216933b3d0b73d62dc300c82ca636af95862
md bused <lanx7hacks@gmail.com>
2025-01-22 15:32:08 -0500

    Part C countryinfo

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { WorldmapComponent } from './worldmap/worldmap.component';
-
-export const routes: Routes = [
-    { path: 'worldmap', component: WorldmapComponent },
-    { path: '' , redirectTo:'/worldmap', pathMatch:'full' }
-];
